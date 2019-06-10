8dcf2f6e1cb5683d9d33b6ee4c03e2bf65f67971
will-chin <50715478+will-chin@users.noreply.github.com>
2019-06-10 12:18:18 -0700

    Create sketch3.js

---
+++ sketch3.js
@@ -0,0 +1,33 @@
+
+function setup() {
+    createCanvas(400, 400);
+    rect(32,43,42,13)
+
+}
+
+function draw() {
+
+    background(220);
+    rect(0,0,44,200)
+    fill(255,204,0);
+    strokeWeight(12.0);
+    strokeCap(SQUARE);
+    line(20, 30);
+    rect(0,44,100,400)
+    fill(52);
+    rect(0,118,50,200)
+    noFill()
+    rect(0,120,50,200)
+    fill(255,204,100);
+    rect(100,0,200,300)
+    fill('red')
+    rect(300,0,100,200)
+    fill('#fae')
+    rect(200,300,100,200)
+    fill(color(0,0,255));
+    rect(300,200,100,300)
+
+
+}
+
+
